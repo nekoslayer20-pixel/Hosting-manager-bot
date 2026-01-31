e8e5018c3e3df9410774645b513c20d862334a33
nekoslayer20-pixel <nekoslayer20@gmail.com>
2026-01-31 15:17:30 +0530

    Create close.js

---
+++ commands/close.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "close",
+  async execute(message) {
+    if (!message.channel.name.startsWith("ticket-"))
+      return message.reply("❌ This is not a ticket channel.");
+
+    message.reply("⏳ Closing ticket in 5 seconds...");
+
+    setTimeout(() => {
+      message.channel.delete();
+    }, 5000);
+  }
+};
